a8bce597886ff02b254b755964f2105e9915ddb0
MorganWilliamson <mwilliamson7695@gmail.com>
2020-11-30 15:34:56 -0600

    GET endpoint written and tested.

--- index.js
+++ index.js
@@ -8,9 +8,51 @@ const server = express();
 
 server.use(express.json());
 
+////////// Fake DB //////////
 let users = [
     { id: shortid.generate(), name: "Son Goku", bio: "Earth's Greatest Defender" },
     { id: shortid.generate(), name: "Vegeta", bio: "Prince of all Saiyans" },
     { id: shortid.generate(), name: "Raditz", bio: "Goku's older brother"}
 ];
 
+////////// Helper Functions //////////
+const UserActions = {
+    getAll() {
+        return users;
+    },
+    getById(id) {
+
+    },
+    createNew(user) {
+
+    },
+    delete(id) {
+
+    },
+    update(id, changes) {
+
+    },
+};
+
+////////// User Endpoints //////////
+server.get("/api/users", (req, res) => {
+    const users = UserActions.getAll()
+    if (users) {
+        res.status(200).json(users)
+    } else {
+        res.status(500).json({ errorMessage: "The users information could not be retrieved." })
+    }
+});
+
+
+
+
+////////// Catch-All Endpoint //////////
+server.use("*", (req, res) => {
+    res.status(404).json({ errorMessage: "The requested information could not be retrieved." });
+});
+
+////////// Server Start //////////
+server.listen(5000, () => {
+    console.log("listening on port 5000")
+})
